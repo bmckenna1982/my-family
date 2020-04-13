90063d6cbd4eef6e66c474f92b8a8fffff8a1341
bmckenna1982 <bmckenna1982@gmail.com>
2020-04-13 19:33:39 -0400

    converting wireframe to components

--- src/Components/app/App.js
+++ src/Components/app/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, withRouter } from 'react-router-dom'
 import Header from '../header/header'
 import './App.css';
 
@@ -58,24 +59,32 @@ class Nav extends Component {
 class UpcomingEvents extends Component {
   render() {
     return (
-      <section >
+      <section className='upcoming-events'>
         <h2>Upcoming Events</h2>
-        <Event />
-        <Event />
+        <div className='calendar-link'>
+          <a href='/calendar'>View Calendar</a>
+        </div>
+        <Event day='Today' time='3pm' title='Baseball practice' />
+        <Event day='Wed 14' time='3pm' title='Baseball practice' />
       </section>
     )
   }
 }
 
 class Event extends Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return (
-      <div className='events-container'>
-        <div className='events-today'>Today 3pm Baseball practice</div>
-        <hr />
-        <div className='events-next'>Wed 2pm Set Crock pot</div>
-        <div className='events-next'>Wed 6pm Baseball practice</div>
+      <div className='event-container'>
+        <div className='event-day'>{this.props.day}</div>
+        <div className='event-time'>{this.props.time}</div>
+        <div className='event-title'>{this.props.title}</div>
       </div>
+      // <div className='events-next'>Wed 2pm Set Crock pot</div>
+      // <div className='events-next'>Wed 6pm Baseball practice</div>
     )
   }
 }
@@ -84,13 +93,13 @@ class Event extends Component {
 class UpcomingTasks extends Component {
   render() {
     return (
-      <section >
+      <section className='upcoming-tasks'>
         <h2>Upcoming Tasks</h2>
         <div className='tasks-container'>
-          <Task />
-          <Task />
-          <Task />
-          <Task />
+          <Task title='Take out trash' points='20' />
+          <Task title='Put away laundry' points='20' />
+          <Task title='Bathe Dogs' points='100' />
+          <Task title='Mow the lawn' points='50' />
         </div>
         <div className='tasks'></div>
         <div className='tasks'></div>
@@ -100,15 +109,30 @@ class UpcomingTasks extends Component {
 }
 
 class Task extends Component {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return (
       <div className='task'>
+        <TaskCheck />
         <div className='task_desc'>
-          <div className='task_title'>Take out trash</div>
-          <div className='point_value'>20pts</div>
+          <div className='task_title'>{this.props.title}</div>
+          {/* <div className='dueDate'>Due: TODAY</div> */}
+          <div className='point_value'>{this.props.points}pts</div>
         </div>
-        <div className='dueDate'>Due: TODAY</div>
       </div>
+    )
+  }
+}
+
+
+class TaskCheck extends Component {
+  render() {
+    return (
+      <input className='taskCheck' type='checkbox' name='task1' id='task1' />
+
     )
   }
 }
@@ -126,4 +150,4 @@ class LatestLists extends Component {
 }
 
 
-export default App;
+export default withRouter(App);
