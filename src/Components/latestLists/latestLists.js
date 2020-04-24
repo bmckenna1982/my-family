3f471a35c06b8c49114c37d43b572558d42c7e0a
bmckennna1982 <bmckenna1982@gmail.com>
2020-04-24 07:54:00 -0400

    updated latestLists to add a drop down add to list entry

--- src/Components/latestLists/latestLists.js
+++ src/Components/latestLists/latestLists.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import AddItemLink from '../addItemLink/addItemLink'
 import AppContext from '../context/appContext'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 
 import './latestLists.css'
@@ -23,8 +23,13 @@ class LatestLists extends Component {
         <h2>Active Lists</h2>
         {this.context.lists.map((list, index) => (
           <div key={index} className='latest-list-container'>
-            <div className='grocery-list'>{list.name}</div>
-            <AddItemLink location='/add-list-item' />
+            <div className='latest-list-wrapper'>
+              <div className='grocery-list'>{list.name}</div>
+              <AddItemLink location='/add-list-item' />
+            </div>
+            <div className='list-input'>
+              <input type='text' name='list-input-title' id='list-input-title' placeholder='List item name' />
+            </div>
           </div>
         ))}
       </section>
