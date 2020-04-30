3b7bc6491a7569c419dfcb5d2ce186edc38ca3da
bmckenna1982 <bmckenna1982@gmail.com>
2020-04-30 08:58:33 -0400

    removed checked property and left as static checkboxes for getting feedback

--- src/Components/taskCheck/taskCheck.js
+++ src/Components/taskCheck/taskCheck.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 
 class TaskCheck extends Component {
-  handleChange = (e) => {
-    e.preventDefault()
-    e.target.checked = !e.target.checked
-  }
+  // handleChange = (e) => {
+  //   e.preventDefault()
+  //   e.target.checked = !e.target.checked
+  // }
 
   render() {
     let itemChecked = this.props.checked
     return (
-      <input className='task-check' type='checkbox' name='task1' id='task1' checked={itemChecked} onChange={this.handleChange} />
+      <input className='task-check' type='checkbox' name='task1' id='task1' onChange={this.handleChange} />
 
     )
   }
