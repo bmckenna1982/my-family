9f8d0cfbf12547dac6ffdc0f7fc1e791bd82c546
bmckenna1982 <bmckenna1982@gmail.com>
2020-05-18 17:58:40 -0400

    created listCheck component and updated logic to control list check boxes

---
+++ src/Components/listCheck/listCheck.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import AppContext from '../context/appContext'
+import ListItemsService from "../services/listItems-service";
+
+import './listCheck.css'
+
+class ListCheck extends Component {
+  static contextType = AppContext
+
+  handleChange = (e) => {
+    e.preventDefault()
+    const listItemToUpdateId = this.props.listItemId
+    //need to also get logged in user and send user_id so points will be added
+    const fieldsToUpdate = {
+      checked: e.target.checked,
+    }
+    ListItemsService.updateListItem(listItemToUpdateId, fieldsToUpdate)
+      .then(() => {
+        ListItemsService.getAllListItems()
+          .then(res => {
+            this.context.setListItems(res)
+          })
+      })
+
+
+  }
+
+  render() {
+    let itemChecked = this.props.checked
+    const listItemId = this.props.taskId
+    console.log('this.props', this.props)
+    return (
+      <input className='listItem-check' type='checkbox' name={`listItem-${listItemId}`} id={`listItem-${listItemId}`} onChange={this.handleChange} checked={itemChecked} />
+
+    )
+  }
+}
+
+export default ListCheck
