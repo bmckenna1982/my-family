29945de82f894d45aa8621f6c15063eadff18276
bmckenna1982 <bmckenna1982@gmail.com>
2020-05-27 10:18:28 -0400

    screenshot on landing page

--- src/Components/landingPage/landingPage.js
+++ src/Components/landingPage/landingPage.js
@@ -16,21 +16,21 @@ class LandingPage extends Component {
         </header>
         <section>
           <header>
-            <h3>Family Calendar</h3>
+            <h2>Family Calendar</h2>
           </header>
-          <p>[<em>placeholder for screenshot of calendar</em>]</p>
+          <img src='https://my-family-app.s3.us-east-2.amazonaws.com/calendarWeek.PNG' alt='calendar screenshot' />
           <p>Keep everyone in sync and get everyone where they need to be with an integrated calendar for all family members</p>
         </section>
         <section>
           <header>
-            <h3>Rewarding tasks system</h3>
+            <h2>Rewarding tasks system</h2>
           </header>
           <p>[<em>placeholder for screenshot of to do list</em>]</p>
           <p>Keep everyone motivated to get things done by awarding points for completing tasks.</p>
         </section>
         <section>
           <header>
-            <h3>Synced lists</h3>
+            <h2>Synced lists</h2>
           </header>
           <p>[<em>placeholder for screenshot of shopping lists</em>]</p>
           <p>Synced lists for creating shopping lists, school supply lists or anything</p>
